2c19ae79c716b9889b69a996939577b466316e10
edirin nelson <nelsonedirin@gmail.com>
2023-08-05 13:13:22 +0100

    QR Code generation with MerchantDetails

---
+++ src/main/java/com/isdservices/paymentwithqrcode/dto/MerchantDetailsDto.java
@@ -0,0 +1,16 @@
+package com.isdservices.paymentwithqrcode.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MerchantDetailsDto {
+    private String bankName;
+    private String accountNumber;
+    private String merchantName;
+}
